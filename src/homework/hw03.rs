f1a99baf056a79852c8b6bc2aaef3bb75aed7a35
Gentlemen991 <trefrex30@gmail.com>
2025-04-16 17:40:42 +0300

    конверт W=28, H=10
    переробив

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,5 +1,6 @@
 fn get_char_at(x: usize, y: usize, width: usize, height: usize) -> char {
     if y == 0 || y == height - 1 || x == 0 || x == width - 1 || x == y || x == width - 1 - y {
+    if y == 0 || y == height - 1 || x == 0 || x == width - 1 || x == y * (width - 1) / (height - 1) || x == (height - 1 - y) * (width - 1) / (height - 1) {
         '*'
     } else {
         ' '
@@ -9,6 +10,8 @@ fn get_char_at(x: usize, y: usize, width: usize, height: usize) -> char {
 fn main() {
     const WIDTH: usize = 50;
     const HEIGHT: usize = 50;
+    const WIDTH: usize = 28;
+    const HEIGHT: usize = 10;
 
     let mut output = String::new();
 
