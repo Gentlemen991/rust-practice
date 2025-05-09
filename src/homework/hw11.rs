844f798a7a958da41a6fdd5547615672a0e90b7e
Gentlemen991 <trefrex30@gmail.com>
2025-05-09 19:07:23 +0300

    Done!

--- src/homework/hw11.rs
+++ src/homework/hw11.rs
@@ -2,35 +2,61 @@ use rand::Rng;
 
 fn gen_random_vector(n: usize) -> Vec<i32> {
     let mut rng = rand::thread_rng();
-    (0..n).map(|_| rng.gen_range(10..=99)).collect()
+    (0..n).map(|_| rng.gen_range(10..100)).collect()
 }
 
 fn min_adjacent_sum(data: &[i32]) -> (usize, i32) {
-    let mut min_sum = data[0] + data[1];
-    let mut index = 0;
-    for i in 1..data.len() - 1 {
+    let mut min_sum = i32::MAX;
+    let mut min_index = 0;
+
+    for i in 0..data.len() - 1 {
         let sum = data[i] + data[i + 1];
         if sum < min_sum {
             min_sum = sum;
-            index = i;
+            min_index = i;
         }
     }
-    (index, min_sum)
+
+    (min_index, min_sum)
 }
 
-fn print_vector(data: &[i32]) {
-    println!("Вектор:");
-    for (i, val) in data.iter().enumerate() {
-        print!("{:>3}{}", val, if i != data.len() - 1 { ", " } else { "\n" });
+fn print_vector_with_min(data: &[i32], min_index: usize) {
+    print!("indexes: ");
+    for i in 0..data.len() {
+        print!("{:>3}.", i);
     }
+    println!();
+
+    print!("data:    ");
+    for val in data {
+        print!("{:>3},", val);
+    }
+    println!();
+
+    print!("indexes: ");
+    for i in 0..data.len() {
+        if i == min_index {
+            print!("\\__");
+        } else if i == min_index + 1 {
+            print!(" __/");
+        } else {
+            print!("     ");
+        }
+    }
+    println!();
+
+    println!(
+        "min adjacent sum={}+{}={} at indexes:{},{}",
+        data[min_index],
+        data[min_index + 1],
+        data[min_index] + data[min_index + 1],
+        min_index,
+        min_index + 1
+    );
 }
 
 fn main() {
-    let vec = gen_random_vector(20);
-    print_vector(&vec);
-    let (index, min_sum) = min_adjacent_sum(&vec);
-    println!(
-        "Мінімальна сума пари: {} + {} = {} (індекси [{}], [{}])",
-        vec[index], vec[index + 1], min_sum, index, index + 1
-    );
+    let data = gen_random_vector(20);
+    let (min_index, _) = min_adjacent_sum(&data);
+    print_vector_with_min(&data, min_index);
 }
